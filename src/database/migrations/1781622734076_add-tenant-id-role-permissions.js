6443de0daa16a4eefaa13d1bcfcca9b3c8faf5c1
dileepk706 <dlpkmr706@gmail.com>
2026-06-16 20:48:07 +0530

    tenant id added to role permissions

---
+++ src/database/migrations/1781622734076_add-tenant-id-role-permissions.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addColumn('role_permissions', {
+    tenant_id: {
+      type: 'uuid',
+      references: 'tenants(id)',
+      notNull: false,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropColumn('role_permissions', 'tenant_id');
+};
--- src/module/role/domain/entities/role.entity.ts
+++ src/module/role/domain/entities/role.entity.ts
@@ -7,4 +7,15 @@ type Role = {
   deleted: Date;
 };
 
+type RolePermission = {
+  id: string;
+  role_id: string;
+  permission_id: string;
+  tenant_id: string;
+  created_at: Date;
+  updated_at: Date;
+  deleted: Date;
+};
+
+export { Role, RolePermission };
 export default Role;
